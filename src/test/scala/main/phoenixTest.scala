3510654941334ab928ef36872f972d0f95d63121
mgpai <64056966+mgpai22@users.noreply.github.com>
2023-09-10 15:01:59 -0400

    add bank ergotree compilation

--- src/test/scala/main/phoenixTest.scala
+++ src/test/scala/main/phoenixTest.scala
@@ -1,8 +1,9 @@
 package main
 
+import contracts.PhoenixContracts
 import execute.Client
 import execute.HodlCalulations.{hodlMintAmountFromERG, hodlPrice}
-import org.ergoplatform.appkit.InputBox
+import org.ergoplatform.appkit.{ErgoContract, InputBox}
 import org.ergoplatform.appkit.impl.InputBoxImpl
 import utils.{ContractCompile, explorerApi}
 
@@ -10,33 +11,48 @@ object phoenixTest extends App {
   private val client: Client = new Client()
   client.setClient
   private val ctx = client.getContext
-  val contractString = contracts.PhoenixContracts.phoenix_v1_hodlcoin_bank.contractScript
+  val contractString =
+    contracts.PhoenixContracts.phoenix_v1_hodlcoin_bank.contractScript
   val compilerObj = new ContractCompile(ctx)
 
   val dummyContractString = "sigmaProp(true)"
   val dummyContract = compilerObj.compileDummyContract(dummyContractString)
 
-  val contract = compilerObj.compileBankContract(contractString, dummyContract)
+  println(dummyContract.getErgoTree.bytesHex)
+
+  val contract = compilerObj.compileProxyContract(
+    contracts.PhoenixContracts.phoenix_v1_hodlcoin_proxy.contractScript,
+    1000000L
+  )
 
   println(contract.getErgoTree.bytes.length)
 
 }
 
-object priceTest extends App {
-  val inputBoxId = "785d02cb5bd1505c793585eed315ac923633fbf9164401d22fa8fd70dd7c4afc"
-  val exp = new explorerApi()
-  val box = new InputBoxImpl(exp.getErgoBoxfromID(inputBoxId))
-    .asInstanceOf[InputBox]
+object bank extends App {
+  private val client: Client = new Client()
+  client.setClient
+  private val ctx = client.getContext
+  val contractString =
+    contracts.PhoenixContracts.phoenix_v1_hodlcoin_bank.contractScript
 
-  val txOperatorFee = 1000000L
-  val minerFee = 1000000L
+  val feeContractScript =
+    PhoenixContracts.phoenix_v1_hodlcoin_feeTest.contractScript
+  val compilerObj = new ContractCompile(ctx)
 
-  val price = hodlPrice(box)
-  val amntERGPaid = 1005968000L + minerFee
-  val amntERGAfterFees = amntERGPaid - txOperatorFee - minerFee
-  val hodlMintAmount = hodlMintAmountFromERG(box, amntERGAfterFees)
+  val dummyContractString = "sigmaProp(true)"
+  val dummyContract = compilerObj.compileDummyContract(dummyContractString)
 
-  println(hodlMintAmount)
+  val feeContract: ErgoContract = compilerObj.compileFeeContract(
+    feeContractScript,
+    1000000L
+  )
 
-  println(price)
+  val contract = compilerObj.compileBankContract(
+    contractString,
+    feeContract
+  )
+
+  println(contract.getErgoTree.bytesHex)
+
 }
