f392cf1bc9cbc0068e09f96e45aceb6ec7f5eb21
mgpai <64056966+mgpai22@users.noreply.github.com>
2024-01-05 00:16:48 -0500

    update for comet

--- src/main/scala/initialize/initialize.scala
+++ src/main/scala/initialize/initialize.scala
@@ -24,11 +24,11 @@ object initialize extends App {
   val outBoxObj = new OutBoxes(ctx)
   val compiler = new ContractCompile(ctx)
 
-  private val feeScript: String =
+  private val feeScript
+      : String = // make sure to change depending on testnet or mainnet
     PhoenixContracts.phoenix_v1_hodlcoin_feeTest.contractScript
 
-  private val phoenixScript
-      : String = // make sure to change depending on testnet or mainnet
+  private val phoenixScript: String =
     PhoenixContracts.phoenix_v1_hodlcoin_bank.contractScript
 
   private val feeContract: ErgoContract = compiler.compileFeeContract(
@@ -48,7 +48,7 @@ object initialize extends App {
 
   val hodlDecimals = 9
   val totalTokenSupply = (97739924 * math.pow(10, hodlDecimals)).toLong
-  val precisionFactor = 1000000L
+  val precisionFactor = 1000000000L
   val minBankValue = 1000000L
   val bankFeeNum = 30L
   val devFeeNum = 3L
@@ -166,6 +166,54 @@ object initialize extends App {
   ).write(contractConfFilePath)
 }
 
+object mintToken extends App {
+  private val client: Client = new Client()
+  client.setClient
+  val ctx = client.getContext
+  val serviceFilePath = "serviceOwner.json"
+  val contractConfFilePath = "contracts.json"
+  val serviceConf = serviceOwnerConf.read(serviceFilePath)
+
+  val walletMnemonic = serviceConf.txOperatorMnemonic
+  val walletMnemonicPw = serviceConf.txOperatorMnemonicPw
+  val txHelper =
+    new TransactionHelper(this.ctx, walletMnemonic, walletMnemonicPw)
+
+  val inputBoxesObj = new InputBoxes(ctx)
+  val outBoxObj = new OutBoxes(ctx)
+
+  val genesisInput =
+    inputBoxesObj
+      .getBoxesById(
+        ""
+      )
+
+  val totalTokenSupply = 21000000000L
+  val decimal = 0
+
+  val hodlTokens = outBoxObj.tokenHelper(
+    genesisInput.head,
+    "Comet",
+    "Comet",
+    totalTokenSupply,
+    decimal
+  )
+
+  val tokenMintOutput =
+    outBoxObj.tokenMintOutBox(hodlTokens, txHelper.senderAddress)
+
+  val unsignedMintTx = txHelper.buildUnsignedTransaction(
+    inputs = genesisInput,
+    outputs = Array(tokenMintOutput),
+    fee = 100000000L
+  )
+
+  val mintTx = txHelper.sendTx(txHelper.signTransaction(unsignedMintTx))
+
+  println("Mint Transaction: " + mintTx)
+
+}
+
 object initializeToken extends App {
   private val client: Client = new Client()
   client.setClient
@@ -190,9 +238,14 @@ object initializeToken extends App {
   private val phoenixScript: String =
     PhoenixContracts.phoenix_v1_hodltoken_bank.contractScript
 
+  val minBankValue =
+    1L // this can change; when deploying you need one unit respective to decimal count
+  // if decimals are 2 then 100L
+  // if decimals are 0 then 1L and so on
+
   val comet = new ErgoToken(
-    "0000ce19a2fafce6cd2f7c573b2bb64ef85d651f89a8e5bf4f3933e3ccebe9f6",
-    1L
+    "0cd8c9f416e5b1ca9f986a7f10a84191dfb85941619e49e53c0dc30ebf83324b",
+    minBankValue
   )
 
   private val feeContract: ErgoContract = compiler.compileFeeTokenContract(
@@ -209,24 +262,28 @@ object initializeToken extends App {
 
   private val proxyAddress = compiler
     .compileProxyContract(
-      PhoenixContracts.phoenix_v1_hodlcoin_proxy.contractScript,
+      PhoenixContracts.phoenix_v1_hodltoken_proxy.contractScript,
       serviceConf.minTxOperatorFee
     )
     .toAddress
 
-  val totalTokenSupply = 1000000000L
-  val precisionFactor = 1000000L
-  val minBankValue = 1L // this can change
+  val totalTokenSupply = 21000000000L
+  val precisionFactor = 1L
   val bankFeeNum = 30L
   val devFeeNum = 3L
 
   private val hodlDecimal = 0
 
+//  val genesisInput =
+//    inputBoxesObj.getInputs(
+//      Array(2 * Parameters.OneErg),
+//      txHelper.senderAddress
+//    )
   val genesisInput =
-    inputBoxesObj.getInputs(
-      Array(2 * Parameters.OneErg),
-      txHelper.senderAddress
-    )
+    inputBoxesObj
+      .getBoxesById(
+        ""
+      )
 
   val singleton = outBoxObj.tokenHelper(
     genesisInput.head,
@@ -256,8 +313,8 @@ object initializeToken extends App {
 
   val hodlTokens = outBoxObj.tokenHelper(
     hodlTokenMintInput,
-    "hodlComet3",
-    "The Phoenix Finance implementation of the hodlToken protocol: hodlComet 3%",
+    "hodlCOMET3",
+    "The Phoenix Finance implementation of the hodlToken protocol: hodlCOMET 3%",
     totalTokenSupply,
     hodlDecimal
   )
@@ -274,12 +331,12 @@ object initializeToken extends App {
 
   val hodlMintTxHash = txHelper.sendTx(hodlMintSignedTx)
 
-  println("Hodl Comet Transaction: " + hodlMintTxHash)
+  println("Hodl COMET Transaction: " + hodlMintTxHash)
 
   val hodlBoxInput = Array(
     inputBoxesObj
       .getBoxesById(
-        "9b3d4a48f1a8bc553da11ef159148ef8b29d52401e2f4a62527b8508fdcda856"
+        ""
       )
       .head,
     singletonMintSignedTransaction.getOutputsToSpend.get(0),
